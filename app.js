52920bec3b9d162cdb76e31a71bc5457224a9195
Faixan6190 <faixan6190@gmail.com>
2023-11-22 16:34:16 +0500

    complete

--- app.js
+++ app.js
@@ -6,50 +6,61 @@ function addNote() {
     alert("Input Field Are Empty");
     return;
   }
+
+  var cardDiv = document.createElement("div");
+  cardDiv.setAttribute("class", "card my-3");
+
+  var cardHeaderDiv = document.createElement("div");
+  cardHeaderDiv.setAttribute(
+    "class",
+    "card-header styling-header fs-4 fw-bold"
+  );
+  cardHeaderDiv.innerHTML = "Note";
+
+  cardDiv.append(cardHeaderDiv);
+
+  var cardBodyDiv = document.createElement("div");
+  cardBodyDiv.setAttribute("class", "card-body");
+
+  cardDiv.append(cardBodyDiv);
+
+  var cardTitleh5 = document.createElement("h5");
+  cardTitleh5.setAttribute("class", "card-title");
+  cardTitleh5.innerHTML = noteInput.value;
+
+  cardBodyDiv.append(cardTitleh5);
+
+  var buttonContainer = document.createElement("button");
+  buttonContainer.setAttribute("class", "mt-3 buttonContainer");
+  cardBodyDiv.append(buttonContainer);
+
+  var editBtn = document.createElement("button");
+  editBtn.innerHTML = "Edit";
+  editBtn.className = "btn btn-secondary mx-2";
+  editBtn.setAttribute("onclick", "editTodo(this)");
+
+  var deleteBtn = document.createElement("button");
+  deleteBtn.innerHTML = "Delete";
+  deleteBtn.className = "btn btn-dark";
+  deleteBtn.setAttribute("onclick", "deleteTodo(this)");
+
+  buttonContainer.append(editBtn);
+  buttonContainer.append(deleteBtn);
+
+  parent.append(cardDiv);
+  noteInput.value = "";
 }
-
-var cardDiv = document.createElement("div");
-cardDiv.setAttribute("class", "card my-3");
-
-var cardHeaderDiv = document.createElement("div");
-cardHeaderDiv.setAttribute("class", "card-header");
-cardHeaderDiv.innerHTML = "Note";
-
-cardDiv.append(cardHeaderDiv);
-
-var cardBodyDiv = document.createElement("div");
-cardBodyDiv.setAttribute("class", "card-body");
-
-cardDiv.append(cardBodyDiv);
-
-var cardTitleh5 = document.createElement("h5");
-cardTitleh5.setAttribute("class", "card-title");
-cardTitleh5.innerHTML = noteInput.value;
-
-cardBodyDiv.append(cardTitleh5);
-
-var buttonContainer = document.createElement("button");
-buttonContainer.setAttribute("class", "mt-3 buttonContainer");
-cardBodyDiv.append(buttonContainer);
-
-var editBtn = document.createElement("button");
-editBtn.innerHTML = "Edit";
-editBtn.className = "btn btn-primary";
-
-var deleteBtn = document.createElement("button");
-deleteBtn.innerHTML = "Delete";
-deleteBtn.className = "btn btn-danger";
-deleteBtn.setAttribute("onclick", "deleteTodo(this)");
-
-buttonContainer.append(editBtn);
-buttonContainer.append(deleteBtn);
-
-console.log(cardDiv);
-parent.append(cardDiv);
-noteInput = "";
 
 function deleteTodo(ele) {
   ele.parentNode.parentNode.parentNode.remove();
+}
+
+function editTodo(elem) {
+  var editValue = prompt(
+    "Enter Edit Value",
+    elem.parentNode.previousElementSibling.innerHTML
+  );
+  elem.parentNode.previousElementSibling.innerHTML = editValue;
 }
 
 function deleteAll() {
